19521d709fdca5d542428c77e3f0878a366894a8
begsense <begsense@gmail.com>
2024-06-28 12:31:03 +0400

    add GeoNews repository

---
+++ GeoNews/AppAndSceneDelegate/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  GeoNews
+//
+//  Created by M1 on 27.06.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+       // let viewModel = MainPageViewModel()
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+}
---
+++ GeoNews/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoNews
+//
+//  Created by M1 on 27.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+
+
+}
+
